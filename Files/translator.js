ab307ce65ea616e24bde44ee54e10db490c9a9ef
xulihang <xulihanghai@163.com>
2024-11-30 21:26:53 +0800

    use other engines in translator.html

---
+++ Files/translator.js
@@ -0,0 +1,181 @@
+let tessWorker;
+let tessLang;
+async function initTess(lang){
+    if (!tessWorker) {
+        await loadLibrary("https://cdn.jsdelivr.net/npm/tesseract.js@5/dist/tesseract.min.js","text/javascript");
+    }
+    if (!tessLang || lang != tessLang) {
+        tessLang = lang;
+        worker = await Tesseract.createWorker(lang, 1, {
+            logger: m => console.log(m),
+        });
+        if (lang.indexOf("vert") != -1) {
+            await worker.setParameters({
+                tessedit_pageseg_mode: Tesseract.PSM.SINGLE_BLOCK_VERT_TEXT,
+            });
+        }
+        
+    }
+}
+
+async function tessOCR(image) {
+    const ret = await worker.recognize(image);
+    let boxes = [];
+    ret.data.lines.forEach(line => {
+        if (line.confidence>10) {
+            const box = {};
+            box["text"] = line.text;
+            box["target"] = "";
+            box["geometry"] = {
+                X: line.bbox.x0,
+                Y: line.bbox.y0,
+                width: line.bbox.x1 - line.bbox.x0,
+                height: line.bbox.y1 - line.bbox.y0
+            }
+            boxes.push(box);
+        }
+    });
+    const imgMap = {};
+    console.log(JSON.parse(JSON.stringify(boxes)))
+    boxes = mergedBoxes(boxes);
+    imgMap["boxes"] = boxes;
+    return imgMap;
+}
+
+function loadLibrary(src,type,id,data){
+    return new Promise(function (resolve, reject) {
+        let scriptEle = document.createElement("script");
+        scriptEle.setAttribute("type", type);
+        scriptEle.setAttribute("src", src);
+        if (id) {
+             scriptEle.id = id;
+        }
+        if (data) {
+            for (let key in data) {
+                scriptEle.setAttribute(key, data[key]);
+            }
+        }
+        document.body.appendChild(scriptEle);
+        scriptEle.addEventListener("load", () => {
+            console.log(src+" loaded")
+            resolve(true);
+        });
+        scriptEle.addEventListener("error", (ev) => {
+            console.log("Error on loading "+src, ev);
+            reject(ev);
+        });
+    });
+}
+
+function clusterBoxes(boxes,baseBox) {
+    let clustered = [];
+    for (let index = boxes.length - 2; index >= 0; index--) {
+        const box = boxes[index];
+        if (areBoxesClose(baseBox,box)) {
+            clustered.splice(0,0,box);
+            boxes.splice(index, 1); //delete the box
+        }
+    }
+    if (clustered.length>0) {
+        clustered.push(baseBox);
+        return clustered;
+    }else{
+        return undefined;
+    }
+}
+
+
+function mergedBoxes(boxes){
+    let grouped = [];
+    let mergedBoxes = [];
+    while (boxes.length > 0) {
+      let box = boxes[boxes.length - 1];
+      let clustered = clusterBoxes(boxes,box);
+      if (clustered) {
+        grouped.push(clustered);
+      } else {
+        grouped.push([box]); //single box as a group
+      }
+      boxes.shift(); //delete the base rect
+    }
+    grouped.forEach(group => {
+        let mergedBox = {};
+        mergedBox = mergedBoxesRect(group);
+        mergedBoxes.push(mergedBox);
+    });
+    return mergedBoxes;
+}
+
+async function translateBoxes(boxes) {
+    for (let index = 0; index < boxes.length; index++) {
+        let box = boxes[index]
+        let text = box["text"];
+        box["target"] = await translateUsingMyMemory(text);
+    }
+}
+
+function textMerged(boxes) {
+    let text = "";
+    boxes.forEach(box => {
+        text = text + box["text"] + "\n";
+    });
+    return text;
+}
+
+function mergedBoxesRect(boxes){
+    let merged = JSON.parse(JSON.stringify(boxes[0]));
+    let minX = boxes[0]["geometry"]["X"];
+    let minY = boxes[0]["geometry"]["Y"];
+    let maxX = 0;
+    let maxY = 0;
+    boxes.forEach(box => {
+        minX = Math.min(minX,box["geometry"]["X"]);
+        minY = Math.min(minY,box["geometry"]["Y"]);
+        maxX = Math.max(maxX,box["geometry"]["X"]+box["geometry"]["width"]);
+        maxY = Math.max(maxY,box["geometry"]["Y"]+box["geometry"]["height"]);
+    });
+    merged["geometry"]["X"] = minX;
+    merged["geometry"]["Y"] = minY;
+    merged["geometry"]["width"] = maxX - minX;
+    merged["geometry"]["height"] = maxY - minY;
+    merged["text"] = textMerged(boxes);
+    return merged;
+}
+
+function areBoxesClose(box1,box2){
+    let merged = mergedBoxesRect([box1,box2]);
+    let mergedRight = merged["geometry"]["X"] + merged["geometry"]["width"];
+    let box1Right = box1["geometry"]["X"] + box1["geometry"]["width"];
+    let box1Width = box1["geometry"]["width"];
+    let box2Right = box2["geometry"]["X"] + box2["geometry"]["width"];
+    let box2Width = box2["geometry"]["width"];
+    if (mergedRight - box1Right < box1Width * 3 || mergedRight - box2Right < box2Width * 3) {
+        return true;
+    }
+    return false;
+}
+
+async function translateUsingMyMemory(source){
+    try {
+        
+        let sourceLang = localStorage.getItem("sourceLang") ?? "ja";
+        let targetLang = localStorage.getItem("targetLang") ?? "en";
+        source = reflowText(sourceLang,source);
+        let url = "https://api.mymemory.translated.net/get?";
+        url = url + "q="+ encodeURIComponent(source);
+        url = url + "&langpair=" + sourceLang + "|" + targetLang;
+        let response = await fetch(url);
+        let o = await response.json();
+        return o.responseData.translatedText;
+    } catch (error) {
+        console.error(error);
+        return "";
+    }
+}
+
+function reflowText(sourceLang,source){
+    if (sourceLang === "ja" || sourceLang === "zh") {
+      return source.replace(/\n/g,"");
+    }
+    return source.replace(/\n/g," ");
+}
